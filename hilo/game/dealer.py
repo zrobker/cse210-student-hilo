0cefc0d8b9c2d756cd22180fb8cc8ca01831a821
awarbler <awoodfor@byui.edu>
2021-10-06 18:13:57 -0500

    added classes to our file and added notes for everyone to review. I need some guidance. I know all info is not correct

---
+++ hilo/game/dealer.py
@@ -0,0 +1,17 @@
+class Dealer:
+
+    def __init__(self):
+        self.card0 = 0
+        self.card1 = 0
+
+
+def  card0(self):
+    pass
+
+def  card1(self):
+    pass
+
+def get_points(self):
+    pass
+
+
---
+++ hilo/game/director.py
@@ -0,0 +1,61 @@
+from game.dealer import Dealer
+
+class Director:
+
+def __init__(self):
+    pass
+''' Constructor
+# initiate the class constructor with self 
+    #   a code template for a person who direct the game. 
+    # The responsiblility of this class of objects is to keep 
+    # track of the score and control the sequence of play.'''
+
+''' Attributes
+    keep_playing (boolean): if player want to keep playing true
+    points(number): counts down to zero
+    dealer: an instance of the class of objects know as the Dealer.'''
+
+def start_game(self):
+    pass
+# initiate start_game 
+#   start the game loop to control the sequence of play
+# Args : self (Director): an instance of director
+
+
+# Use While Loop - 
+#   self. keep_playing () while true, self .get_cards(), 
+#   ?self.get_input() , get_output and  ?self.do_outputs()
+
+
+def get_cards():
+# define get_cards
+# Generates 2 new cards,
+#  and prepares them for their n
+        """get the cards  """
+    self.dealer.card0()
+    self.dealer.card1()
+
+
+
+def run_game():
+    """print(f"The card is: {card0.card}")  """
+pass
+
+
+def do_updates(self):
+        """Updates the important game information for each round of play. In 
+        this case, that means updating the points
+
+        Args:
+            self (Director): An instance of Director."""
+pass   
+""" #calls checkres with current variables
+        points = checkRes(card0, card1, points)
+        print(f"Your score is: {player.points}")
+        #asks user if they want to keep playing
+        if input("Keep Playing? [y/n]") == "n":
+            keepPlaying = False
+        print("")""" 
+
+
+
